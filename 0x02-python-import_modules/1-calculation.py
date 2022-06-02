8c50b435dbbf87935215ee1940e81f3dba411877
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-02 19:34:24 +0100

    correction 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    sys.path.append("calculator_1")
-    import calculator_1 as func
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, func.add(a, b)))
-    print("{} - {} = {}".format(a, b, func.sub(a, b)))
-    print("{} * {} = {}".format(a, b, func.mul(a, b)))
-    print("{} / {} = {}".format(a, b, func.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
