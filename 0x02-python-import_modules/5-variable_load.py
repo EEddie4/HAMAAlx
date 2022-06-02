4e55020f6e6e08c6433ad64ae8f86b73c43d92b6
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-02 15:05:54 +0100

    5. Everything can be imported correction 2

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-import variable_load_5
 
 if __name__ == "__main__":
-    print(variable_load_5.a)
+    import sys
+    sys.path.append("variable_load_5")
+    import variable_load_5 as i
+    print(i.a)
--- 0x02-python-import_modules/variable_load_5.py
+++ 0x02-python-import_modules/variable_load_5.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
 a = 98
 """Simple variable
 """
