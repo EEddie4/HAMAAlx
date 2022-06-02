f48ab27e063a4f410fe96f313170c8b6a025328e
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-02 19:37:46 +0100

    correction encore

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    sys.path.append("add_0")
-    import add_0 as func
+    from add_0 import add
     a = 1
     b = 2
 
-    print("{} + {} = {}".format(a, b, func.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
