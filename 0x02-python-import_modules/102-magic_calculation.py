6251e4fbb04f2b35f52fe8230169910e347656c0
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-03 11:51:27 +0100

    0. Print a list of integers

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import magic_calculation_102
+from magic_calculation_102 import add, sub
 
 
 def magic_calculatrice(a, b):
-    pass
+    if a < b:
+        c = add(a, b)
+        for loop in range(38, 98):
+            
