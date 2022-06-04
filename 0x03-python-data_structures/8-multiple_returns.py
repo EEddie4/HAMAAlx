50725b94d00963c70abdac882dd1221c0644e138
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-04 13:52:56 +0100

    8. More returns! correction 2

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,3 @@
 def multiple_returns(sentence):
     len_s = len(sentence)
-    if len_s == 0:
-        return (len_s, None)
-    return(len_s, sentence[0])
+    return((len_s, sentence[0]) if len_s != 0 else (None, sentence[0]))
