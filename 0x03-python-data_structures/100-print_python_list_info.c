007b228060b0058c6a71f73e41d494e935ec9df5
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-05 15:31:21 +0100

    correction

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,18 +1,28 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "Python.h"
 
 /**
- * print_python_list_info - check the code for
- * @p: object
- *
- * Return: Always 0.
+ * print_python_list_info - a pthon extension function
+ * @p: the PtObject
  */
+
 void print_python_list_info(PyObject *p)
 {
-	printf("[*] Size of the Python list = ");
-	printf("[*] Allocated = ");
-	return (0);
+	PyListObject *list;
+	Py_ssize_t size, i;
+	PyObject *object;
+	struct _typeobject *type;
+
+	if (strcmp(p->ob_type->tp_name, "list") == 0)
+	{
+		list = (PyListObject *)p;
+		size = list->ob_base.ob_size;
+		printf("[*] Size of the Python List = %ld\n", size);
+		printf("[*] Allocated = %ld\n", list->allocated);
+		for (i = 0; i < size; i++)
+		{
+			object = list->ob_item[i];
+			type = object->ob_type;
+			printf("Element %ld: %s\n", i, type->tp_name);
+		}
+	}
 }
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return(None)
-    maxe = my_list[0]
-    for n in my_list:
-        if n > maxe:
-            maxe = n
-    return (maxe)
+    if my_list:
+        maxe = my_list[0]
+        for n in my_list:
+            if n > maxe:
+                maxe = n
+        return (maxe)
+    return (None)
