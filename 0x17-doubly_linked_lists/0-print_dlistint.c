2aafdc9b239e0b03342b5c7094e34ca083cac4de
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-09 10:56:09 +0100

    0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint -  a function
+ * @h: the head of list
+ *
+ * Return: number element of the link list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nb = 0;
+	const dlistint_t *ptr;
+
+	ptr = h;
+	if (ptr != NULL)
+	{
+		while (ptr != NULL)
+		{
+			printf("%d \n", ptr->n);
+			nb++;
+			ptr = ptr->next;
+		}
+	}
+
+	return (nb);
+}
+
