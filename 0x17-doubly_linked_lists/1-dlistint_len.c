fc896c2ddbf761c749421c1243c49110b6da14a3
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-09 11:35:48 +0100

    1. List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint -  a function
+ * dlistint_len -  a function
  * @h: the head of list
  *
  * Return: number element of the link list
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nb = 0;
 	const dlistint_t *ptr;
@@ -19,7 +18,6 @@ size_t print_dlistint(const dlistint_t *h)
 	ptr = h;
 	while (ptr != NULL)
 	{
-		printf("%d \n", ptr->n);
 		nb++;
 		ptr = ptr->next;
 	}
