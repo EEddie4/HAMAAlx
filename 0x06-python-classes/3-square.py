77759fa846127e4ab140b42b7699ab53ffd0e87f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-21 23:11:21 +0100

    3-square correction 1

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,19 @@
+
+
 #!/usr/bin/python3
+"""Define a class Square."""
+
+
 class Square:
-    """square class"""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """initialization method"""
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
-
-    def area(self):
-        """square value"""
-        return (self.__size**2)
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
