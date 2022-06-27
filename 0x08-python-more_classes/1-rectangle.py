03a2d6ad1a09a9e972651b1d2a965f90f45b5477
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-27 18:47:57 +0100

    1. Real definition of a rectangle correction 7

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,9 +21,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise TypeError("height must be >= 0")
         self._Rectangle__height = value
 
@@ -34,8 +34,8 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise TypeError("width must be >= 0")
         self._Rectangle__width = value
