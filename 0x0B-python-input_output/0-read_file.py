ec67ba727f02ec0ed6e9d0c54f0d894a76da4640
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-07-05 12:54:19 +0100

    0. Read file correction 2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 def read_file(filename=""):
     """The read file function"""
 
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
