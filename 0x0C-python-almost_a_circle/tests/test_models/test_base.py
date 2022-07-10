d538b72662c86f8798fa023c9c0362453562e5d3
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-07-10 22:21:24 +0100

    0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,7 +7,7 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     def id(self):
         u = Base(id=15)
-	self.assertEqual(u.id,15)
+        self.assertEqual(u.id,15)
 
 if __name__ == '__main__':
     unittest.main()
