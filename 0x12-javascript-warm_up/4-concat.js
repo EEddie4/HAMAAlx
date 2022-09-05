bbdef9f07daddb60a470b78ee8c5cb220b06e92b
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-05 14:19:23 +0100

    4. Create a sentence correction 1

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-let arg1 = 'undefined';
-let arg2 = 'undefined';
-if (process.argv[2]) {
-  arg1 = process.argv[2];
-}
-if (process.argv[3]) {
-  arg2 = process.argv[3];
-}
-console.log(arg1, ' is ', arg2);
+
+console.log(process.argv[2], ' is ', process.argv[3]);
