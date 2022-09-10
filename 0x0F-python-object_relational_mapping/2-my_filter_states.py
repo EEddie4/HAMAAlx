f38085648ea6baaf4d2bfbe986200bbee68b76d4
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-10 09:01:18 +0100

    correction dans la boucle de 2.my....

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,9 +20,10 @@ if __name__ == "__main__":
             db=database,
             charset="utf8")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(motif))
+        cur.execute("SELECT * FROM states WHERE name = '{}'".format(motif))
         query_rows = cur.fetchall()
         for row in query_rows:
-            print(row)
+            if row[1] == motif:
+                print(row)
         cur.close()
         conn.close()
