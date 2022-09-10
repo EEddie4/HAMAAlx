f1c3eb03fbc165e64d0630f344976e8ac248b8e8
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-10 13:25:26 +0100

    correction

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,12 +19,16 @@ if __name__ == "__main__":
             db=database,
             charset="utf8")
         cur = conn.cursor()
-        sql = "SELECT name FROM cities WHERE state_id = "
-        sql = sql + "(SELECT id FROM states WHERE name = "
-        sql = sql + "'{}'".format(sys.argv[4]) + " LIMIT 1) ORDER BY id ASC"
+        sql = "SELECT cities.name FROM cities INNER \
+        JOIN states ON cities.state_id = states.id \
+        WHERE states.name LIKE '{}' ORDER BY \
+        cities.id".format(sys.argv[4])
         cur.execute(sql)
         query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
+        _len = len(query_rows)
+        if _len != 0:
+            for i in range(_len - 1):
+                print("{}, ".format(query_rows[i][0]), end="")
+            print(query_rows[_len-1][0])
         cur.close()
-        conn.close() 
+        conn.close()
