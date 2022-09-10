577bfa170658cf78be78c2d0d1a8b8c14664e3f4
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-10 09:08:28 +0100

    essai de correction de 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,12 +3,14 @@
 python script that ---
 """
 
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    """Representation of a state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128))
+    name =  Column(String(128), nullable = False)
