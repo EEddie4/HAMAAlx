d9a72f6e17d1c29afb612ba498f1eae20c53bfe4
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-09-25 11:59:51 +0100

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,16 @@
-#!/bin/bash
-# curl sends GET req to URL, displays response body
+#!/usr/bin/python3
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
 
+
+if __name__ == '__main__':
+    from urllib.request import urlopen
+    url = "https://alx-intranet.hbtn.io/status"
+    with urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(content)))
+        print("\t - content: {}".format(content))
+        print("\t - utf8 content: {}".format(content.decode('utf-8')))
