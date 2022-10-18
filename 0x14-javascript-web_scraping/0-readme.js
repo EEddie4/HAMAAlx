b435d5a74c5e4570bad140932a0adf7b0c04f530
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-18 05:30:34 +0100

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const filename = process.argv.slice(2)[0];
+const fs = require('fs');
+
+fs.readFile(filename, (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
