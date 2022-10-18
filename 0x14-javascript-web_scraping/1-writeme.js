43cd380dae6a97e1bf80142502af9df73b728c4e
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-18 10:23:34 +0100

    correction

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const filename = process.argv.slice(2)[0];
-const chaine = process.argv.slice(2)[1] + '\n';
+const chaine = process.argv.slice(2)[1];
 const fs = require('fs');
 
 fs.writeFile(filename, chaine, (err) => {
